b414d47ecc96c65929670e49bbeb72916f4c7924
AnkitRajCode <rajankit432@gmail.com>
2021-11-17 22:56:01 +0530

    Navbar webview done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,9 @@ import Routes from './Routes/Router'
 
 function App() {
   return (
-<<<<<<< HEAD
     <>
       <Routes/>
     </>
-=======
-    <div>
-      <h1 className="text-center text-4xl text-red-700">Portfolio</h1>
-    </div>
->>>>>>> ce2971d40a5b142b5030aa3a11f1bf87b0201d19
   );
 }
 
