a79dbe75c77abb2bd787691bfdc748e837c243a8
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-11-22 16:15:09 +0530

    journey and header animation

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -13,6 +13,7 @@ import TechAndTools from '../components/TechAndTools'
 // Animation Imports
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { Fade, Zoom } from 'react-reveal';
 import ProjectLanding from '../components/ProjectLanding'
 import Footer from '../components/Footer'
 import BlogsLanding from '../components/BlogsLanding'
@@ -40,19 +41,24 @@ const Landing = () => {
                 <div className="Landing-header">
                     <Particles color="#d400ff" type="cobweb" />
                     <div className="typingEffect">
-                        <p>~Hello World~</p>
-                       
-                        <p>I am Ashish Kumar Mishra</p>
+                        <Fade down  duration = {3000} delay = {500}>
+                            <p>~Hello World~</p>
+                        </Fade>
+                        <Fade up duration = {3000} delay = {500}>
+                            <p>I am Ashish Kumar Mishra</p>
+                        </Fade>
+                        <Zoom duration = {5000} delay = {300}>
                         <Typewriter
-                            onInit={(typewriter) => {
-                                typewriter.typeString('Frontend | Android | Data Science | UI/UX')
+                        onInit={(typewriter) => {
+                            typewriter.typeString('Frontend | Android | Data Science | UI/UX')
                                 .callFunction(() => {
                                     console.log('String typed out!');
                                 })
                                 .pauseFor(2500)
                                 .start();
-                            }}
-                        />
+                        }}
+                    />
+                        </Zoom>
                     </div>
                 </div>
 
@@ -60,11 +66,11 @@ const Landing = () => {
                 <div className="px-24 py-28 overflow-hidden">
                     <div className="Landing-Heading md:ml-14" data-aos="fade-in" data-aos-duration="9000">ABOUT</div>
                     <div className="flex md:flex-row">
-                        <div className="w-3/4 text-2xl pr-28 text-justify" style={{color:"#B5B4B4"}} data-aos="slide-right" data-aos-duration="9000">
-                        Hello Folks, I am Ashish Kumar Mishra. I am a hardworking, self-motivated, and well-organized person, sincerely dedicated to my allocated job. I am an IT undergraduate scholar with a penchant for exploring various innovations that are emerging in the technical field. Web Development fascinates me the most. I have acquired skills in Android development and SEO and currently, I have lessons in Data Science and working on research papers. I have obtained certification in UI/UX and terminated with designing websites and projects. I am brushing up my skills to attain more perfection and intend to generate newness in the field of Information and Technology.
+                        <div className="w-3/4 text-2xl pr-28 text-justify" style={{ color: "#B5B4B4" }} data-aos="slide-right" data-aos-duration="9000">
+                            Hello Folks, I am Ashish Kumar Mishra. I am a hardworking, self-motivated, and well-organized person, sincerely dedicated to my allocated job. I am an IT undergraduate scholar with a penchant for exploring various innovations that are emerging in the technical field. Web Development fascinates me the most. I have acquired skills in Android development and SEO and currently, I have lessons in Data Science and working on research papers. I have obtained certification in UI/UX and terminated with designing websites and projects. I am brushing up my skills to attain more perfection and intend to generate newness in the field of Information and Technology.
                         </div>
                         <div className="w-1/4" data-aos="slide-left" data-aos-duration="9000">
-                            <img src={Profile} alt="Profile" style={{width:"220px"}} />
+                            <img src={Profile} alt="Profile" style={{ width: "220px" }} />
                         </div>
                     </div>
                 </div>
--- src/components/JourneyLanding.js
+++ src/components/JourneyLanding.js
@@ -2,30 +2,46 @@ import React from 'react'
 import workExp from '../icons/workExp.png'
 import KiitLogo from '../icons/KiitLogo.png'
 import { Link } from 'react-router-dom'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { Zoom, Flip } from 'react-reveal';
+
+AOS.init({
+    duration: 2000,
+    delay: 5000,
+    easing: 'ease-in-out',
+});
+AOS.refresh();
 
 export default function JourneyLanding() {
     return (
-        <div className="Landing-Journey px-24 py-10">
+        <div className="Landing-Journey px-24 py-10 overflow-hidden">
 
-            <div className="Landing-Heading text-center mb-10">JOURNEY</div>
+            <div data-aos="fade-down" data-aos-easing="linear" className="Landing-Heading text-center mb-10">JOURNEY</div>
 
             <div className="flex flex-row">
                 {/* Journey-Work */}
                 <div className="w-1/2">
 
                     <div className="Journey-Heading flex flex-row items-center">
-                        <img src={workExp} alt="work" style={{width:"50px"}} />
-                        <div className="text-4xl text-white ml-8" style={{color:"#ACACAC"}}>Work Experience</div>
+                        <img src={workExp} alt="work" style={{ width: "50px" }} />
+                        <div className="text-4xl text-white ml-8" style={{ color: "#ACACAC" }}>Work Experience</div>
                     </div>
 
                     <div className="Timeline h-32"></div>
 
                     <div className="Journey-Heading  flex flex-row items-center -mt-20">
-                        <img src={KiitLogo} alt="work" style={{width:"50px"}} />
-                        <div className="text-white ml-8" >
+                        <div data-aos="fade-up">
+                            <Zoom>
+                                <img src={KiitLogo} alt="work" style={{ width: "200px" }} />
+                            </Zoom>
+                        </div>
+                        <div data-aos="fade-right" className="text-white ml-8 overflow-hidden" >
                             <div className="text-xl">Web & Android Developer & UI/UX Designer(GFG-KIIT)</div>
-                            <div className="text-lg py-2"style={{color:"#ACACAC"}}>December-2020 - Present</div>
-                            <div className="text-base pr-24  text-justify " style={{color:"#B5B4B4"}}>I am responsible for making projects on the above mentioned domains and helping others in the club with the same. I am also responsible for leading some of the major events which the club puts forward for all the students and developers around the University.</div>
+                            <div className="text-lg py-2" style={{ color: "#ACACAC" }}>December-2020 - Present</div>
+                            <Flip x duration={5000} delay={2000}>
+                                <div className="text-base pr-24  text-justify " style={{ color: "#B5B4B4" }}>I am responsible for making projects on the above mentioned domains and helping others in the club with the same. I am also responsible for leading some of the major events which the club puts forward for all the students and developers around the University.</div>
+                            </Flip>
                         </div>
                     </div>
 
@@ -35,18 +51,24 @@ export default function JourneyLanding() {
                 <div className="w-1/2">
 
                     <div className="Journey-Heading  flex flex-row items-center">
-                        <img src={workExp} alt="work" style={{width:"50px"}} />
-                        <div className="text-4xl text-white ml-8" style={{color:"#ACACAC"}}>Education</div>
+                        <img src={workExp} alt="work" style={{ width: "50px" }} />
+                        <div className="text-4xl text-white ml-8" style={{ color: "#ACACAC" }}>Education</div>
                     </div>
 
                     <div className="Timeline h-32"></div>
 
                     <div className="Journey-Heading flex flex-row items-center -mt-20">
-                        <img src={KiitLogo} alt="work" style={{width:"50px"}} />
-                        <div className="text-white ml-8"  >
+                        <div data-aos="fade-up">
+                            <Zoom>
+                                <img src={KiitLogo} alt="work" style={{ width: "200px" }} />
+                            </Zoom>
+                        </div>
+                        <div data-aos="fade-left" className="text-white ml-8 overflow-hidden"  >
                             <div className="text-xl">Web & Android Developer & UI/UX Designer(GFG-KIIT)</div>
-                            <div className="text-lg py-2" style={{color:"#ACACAC"}}>December-2020 - Present</div>
-                            <div className="text-base pr-24  text-justify" style={{color:"#B5B4B4"}}>I am responsible for making projects on the above mentioned domains and helping others in the club with the same. I am also responsible for leading some of the major events which the club puts forward for all the students and developers around the University.</div>
+                            <div className="text-lg py-2" style={{ color: "#ACACAC" }}>December-2020 - Present</div>
+                            <Flip x duration={5000} delay={2000}>
+                            <div className="text-base pr-24  text-justify" style={{ color: "#B5B4B4" }}>I am responsible for making projects on the above mentioned domains and helping others in the club with the same. I am also responsible for leading some of the major events which the club puts forward for all the students and developers around the University.</div>
+                            </Flip>
                         </div>
                     </div>
 
