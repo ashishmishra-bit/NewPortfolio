3cf1721661f72b17522d1d5bd68cd52f4bcd7b7f
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-11-22 21:33:01 +0530

    certification page content update

--- src/Pages/Achievements.jsx
+++ src/Pages/Achievements.jsx
@@ -5,6 +5,9 @@ import HackerRankLogo from '../images/HackerRankLogo.png'
 import credentials from '../icons/credentials.png'
 import Footer from '../components/Footer'
 import Sidebar from '../components/Sidebar'
+import Coursera from '../images/coursera.png'
+import Eudonix from '../images/eudonix.png'
+import Udemy from '../images/udemy.png'
 
 export default function Achievements() {
     return (
@@ -28,11 +31,11 @@ export default function Achievements() {
                     </div>
                     <div className="w-3/4 flex flex-col justify-center">
                         <div className="text-5xl text-white">Problem Solving (Basic) Certificate</div>
-                        <div className="text-lg text-white my-2">HackerRank</div>
-                        <div className="text-lg text-white mb-2">Issue September , 2021 - No expiration date</div>
-                        <a href="https://www.google.com" className="text-lg text-white flex flex-row items-center">
+                        <div className="description text-lg my-2">HackerRank</div>
+                        <div className=" description text-lg  mb-2">Issue September , 2021 - No expiration date</div>
+                        <a href="https://www.hackerrank.com/certificates/2d7d83a79116" className="text-lg text-white flex flex-row items-center" target="_blank">
                             <img src={credentials} alt="credentials" style={{width:"20px",height:"20px"}} />
-                            <span className="ml-2">View Credential</span>
+                            <span className=" description ml-2">View Credential</span>
                         </a>
                     </div>
                 </div>
@@ -42,68 +45,98 @@ export default function Achievements() {
                         <img src={HackerRankLogo} alt="HackerRankLogo" style={{width:"200px"}} />
                     </div>
                     <div className="w-3/4 flex flex-col justify-center">
-                        <div className="text-5xl text-white">Problem Solving (Basic) Certificate</div>
-                        <div className="text-lg text-white my-2">HackerRank</div>
-                        <div className="text-lg text-white mb-2">Issue September , 2021 - No expiration date</div>
-                        <a href="https://www.google.com" className="text-lg text-white flex flex-row items-center">
+                        <div className="text-5xl text-white">Problem Solving (Intermediate) Certificate</div>
+                        <div className="description text-lg  my-2">HackerRank</div>
+                        <div className="description text-lg mb-2">Issue September , 2021 - No expiration date</div>
+                        <a href="https://www.hackerrank.com/certificates/d682e68f64f8" className="text-lg text-white flex flex-row items-center" target="_blank">
                             <img src={credentials} alt="credentials" style={{width:"20px",height:"20px"}} />
-                            <span className="ml-2">View Credential</span>
+                            <span className="description  ml-2">View Credential</span>
                         </a>
                     </div>
                 </div>
                 <div className="flex flex-row mx-28 mt-20">
                     <div className="w-1/4">
-                        <img src={HackerRankLogo} alt="HackerRankLogo" style={{width:"200px"}} />
+                        <img src={Eudonix} alt="Eudonix Logo" style={{width:"200px"}} />
                     </div>
                     <div className="w-3/4 flex flex-col justify-center">
-                        <div className="text-5xl text-white">Problem Solving (Basic) Certificate</div>
-                        <div className="text-lg text-white my-2">HackerRank</div>
-                        <div className="text-lg text-white mb-2">Issue September , 2021 - No expiration date</div>
-                        <a href="https://www.google.com" className="text-lg text-white flex flex-row items-center">
+                        <div className="text-5xl text-white">Socket Programming</div>
+                        <div className="description text-lg my-2">Eduonix Learning Solutions Pvt Ltd</div>
+                        <div className="text-lg description  mb-2">Issued August , 2021 - No expiration date</div>
+                        <a href="https://www.eduonix.com/certificate/2cc7d367ae" className="text-lg text-white flex flex-row items-center" target="_blank">
                             <img src={credentials} alt="credentials" style={{width:"20px",height:"20px"}} />
-                            <span className="ml-2">View Credential</span>
+                            <span className="description  ml-2">View Credential</span>
+                        </a>
+                    </div>
+                </div>
+                <div className="flex flex-row mx-28 mt-20">
+                    <div className="w-1/4 ">
+                        <img src={Coursera} alt="Coursera Logo" style={{width:"200px"}} />
+                    </div>
+                    <div className="w-3/4 flex flex-col justify-center">
+                        <div className="text-5xl text-white">AWS Fundamentals: Going Cloud-Native</div>
+                        <div className="description text-lg my-2">Coursera</div>
+                        <div className="description text-lg mb-2">Issue June , 2020 - No expiration date</div>
+                        <a href="https://www.coursera.org/account/accomplishments/certificate/E4DSTYJMXKUJ" className="text-lg text-white flex flex-row items-center" target="_blank">
+                            <img src={credentials} alt="credentials" style={{width:"20px",height:"20px"}} />
+                            <span className="description ml-2">View Credential</span>
                         </a>
                     </div>
                 </div>
                 <div className="flex flex-row mx-28 mt-20">
                     <div className="w-1/4">
-                        <img src={HackerRankLogo} alt="HackerRankLogo" style={{width:"200px"}} />
+                        <img src={Udemy} alt="Udemy Logo" style={{width:"200px"}} />
                     </div>
                     <div className="w-3/4 flex flex-col justify-center">
-                        <div className="text-5xl text-white">Problem Solving (Basic) Certificate</div>
-                        <div className="text-lg text-white my-2">HackerRank</div>
-                        <div className="text-lg text-white mb-2">Issue September , 2021 - No expiration date</div>
-                        <a href="https://www.google.com" className="text-lg text-white flex flex-row items-center">
+                        <div className="text-5xl text-white">Complete Machine Learning</div>
+                        <div className="description text-lg my-2">Udemy</div>
+                        <div className="text-lg description  mb-2">Issue April , 2020 - No expiration date</div>
+                        <a href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-0ed139a0-4c15-4742-a97e-1fd3750a9bb5.pdf" className="text-lg text-white flex flex-row items-center" target="_blank">
                             <img src={credentials} alt="credentials" style={{width:"20px",height:"20px"}} />
-                            <span className="ml-2">View Credential</span>
+                            <span className="description ml-2">View Credential</span>
                         </a>
                     </div>
                 </div>
                 <div className="flex flex-row mx-28 mt-20">
                     <div className="w-1/4">
-                        <img src={HackerRankLogo} alt="HackerRankLogo" style={{width:"200px"}} />
+                        <img src={Coursera} alt="Coursera Logo" style={{width:"200px"}} />
                     </div>
                     <div className="w-3/4 flex flex-col justify-center">
-                        <div className="text-5xl text-white">Problem Solving (Basic) Certificate</div>
-                        <div className="text-lg text-white my-2">HackerRank</div>
-                        <div className="text-lg text-white mb-2">Issue September , 2021 - No expiration date</div>
-                        <a href="https://www.google.com" className="text-lg text-white flex flex-row items-center">
+                        <div className="text-5xl text-white">SEO Fundamentals</div>
+                        <div className="text-lg description  my-2">Coursera</div>
+                        <div className="text-lg description  mb-2">Issue July , 2020 - No expiration date</div>
+                        <a href="https://www.coursera.org/account/accomplishments/verify/3S2BWL95EQMN?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=pdf_header_button&utm_product=course" className="text-lg text-white flex flex-row items-center" target="_blank">
                             <img src={credentials} alt="credentials" style={{width:"20px",height:"20px"}} />
-                            <span className="ml-2">View Credential</span>
+                            <span className="description ml-2">View Credential</span>
                         </a>
                     </div>
                 </div>
+
                 <div className="flex flex-row mx-28 mt-20">
                     <div className="w-1/4">
-                        <img src={HackerRankLogo} alt="HackerRankLogo" style={{width:"200px"}} />
+                        <img src={Coursera} alt="Coursera Logo" style={{width:"200px"}} />
                     </div>
                     <div className="w-3/4 flex flex-col justify-center">
-                        <div className="text-5xl text-white">Problem Solving (Basic) Certificate</div>
-                        <div className="text-lg text-white my-2">HackerRank</div>
-                        <div className="text-lg text-white mb-2">Issue September , 2021 - No expiration date</div>
-                        <a href="https://www.google.com" className="text-lg text-white flex flex-row items-center">
+                        <div className="text-5xl text-white">Introduction to Search Engine Optimisation</div>
+                        <div className="text-lg description  my-2">Coursera</div>
+                        <div className="text-lg description  mb-2">Issue June , 2020 - No expiration date</div>
+                        <a href="https://www.coursera.org/account/accomplishments/certificate/TGWSGCSH7RNT" className="text-lg text-white flex flex-row items-center" target="_blank">
                             <img src={credentials} alt="credentials" style={{width:"20px",height:"20px"}} />
-                            <span className="ml-2">View Credential</span>
+                            <span className="description ml-2">View Credential</span>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="flex flex-row mx-28 mt-20">
+                    <div className="w-1/4">
+                        <img src={Coursera} alt="Coursera Logo" style={{width:"200px"}} />
+                    </div>
+                    <div className="w-3/4 flex flex-col justify-center">
+                        <div className="text-5xl text-white">Fundamentals Of Graphics Design</div>
+                        <div className="text-lg description  my-2">Coursera</div>
+                        <div className="text-lg description  mb-2">Issue July , 2020 - No expiration date</div>
+                        <a href="https://www.coursera.org/account/accomplishments/certificate/4AQ2KAANT7ML" className="text-lg text-white flex flex-row items-center" target="_blank">
+                            <img src={credentials} alt="credentials" style={{width:"20px",height:"20px"}} />
+                            <span className="description ml-2">View Credential</span>
                         </a>
                     </div>
                 </div>
--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -64,7 +64,7 @@ const Landing = () => {
 
                 {/* ABOUT SECTION */}
                 <div className="px-24 py-28 overflow-hidden">
-                    <div className="Landing-Heading md:ml-14" data-aos="fade-in" data-aos-duration="9000">ABOUT</div>
+                    <div className="Landing-Heading md:ml-14 lg:ml-8 " data-aos="fade-in" data-aos-duration="9000">ABOUT</div>
                     <div className="flex md:flex-row">
                         <div className="w-3/4 text-2xl pr-28 text-justify" style={{ color: "#B5B4B4" }} data-aos="slide-right" data-aos-duration="9000">
                             Hello Folks, I am Ashish Kumar Mishra. I am a hardworking, self-motivated, and well-organized person, sincerely dedicated to my allocated job. I am an IT undergraduate scholar with a penchant for exploring various innovations that are emerging in the technical field. Web Development fascinates me the most. I have acquired skills in Android development and SEO and currently, I have lessons in Data Science and working on research papers. I have obtained certification in UI/UX and terminated with designing websites and projects. I am brushing up my skills to attain more perfection and intend to generate newness in the field of Information and Technology.
