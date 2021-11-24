6d2c975fa71c439d0c5b2a2fb301f20f62f45dd8
AnkitRajCode <rajankit432@gmail.com>
2021-11-24 15:56:30 +0530

    Competetive programming added in achivements

--- src/Pages/Achievements.jsx
+++ src/Pages/Achievements.jsx
@@ -3,6 +3,13 @@ import '../css/Achievements.css'
 import AchievementsHeader from '../images/AchievementsHeader.png'
 import HackerRankLogo from '../images/HackerRankLogo.png'
 import credentials from '../icons/credentials.png'
+import problemSolvingBadge from '../icons/problemSolvingBadge.png'
+import DaysOfCodingBadge from '../icons/30DaysOfCodingBadge.png'
+import star from '../icons/star.svg'
+import CppBadge from '../icons/CppBadge.png'
+import JavaBadge from '../icons/JavaBadge.png'
+import LeetcodeLogo from '../icons/LeetcodeLogo.png'
+import CodeChiefLogo from '../icons/CodeChiefLogo.png'
 import Footer from '../components/Footer'
 import Sidebar from '../components/Sidebar'
 import Coursera from '../images/coursera.png'
@@ -24,6 +31,144 @@ export default function Achievements() {
 
                 {/* BODY SECTION */}
                 <div className="Landing-Heading TechAndTools-Heading text-center mb-10 uppercase">Achievements</div>
+
+                {/* COMPETETIVE PROGRAMMING ACHIVEMENT */}
+                <div className="flex flex-row justify-between mx-28 mb-16" style={{color:"#979797"}}>
+                    {/* HACKERRANK CARD */}
+                    <div className="competetive-card">
+                        <div className="flex flex-row items-center">
+                            <img src={HackerRankLogo} alt="HackerRankLogo" style={{width:"40px",height:"40px"}} />
+                            <span className="text-lg font-semibold ml-5">HackerRank</span>
+                        </div>
+
+                        <p  className="text-lg font-semibold mt-5 mb-2">Badges</p>
+                        <div className="flex flex-row justify-center">
+                            <img src={problemSolvingBadge} alt="problemSolvingBadge" style={{width:"45px",height:"35px"}} />
+                            <img src={CppBadge} alt="CppBadge" className="ml-3" style={{width:"45px",height:"35px"}} />
+                            <img src={JavaBadge} alt="JavaBadge" className="ml-3" style={{width:"45px",height:"35px"}} />
+                            <img src={DaysOfCodingBadge} alt="DaysOfCodingBadge" className="ml-3" style={{width:"45px",height:"35px"}} />
+                        </div>
+
+                        <p  className="text-lg font-semibold mt-5 mb-2">Certificates</p>
+                        <a href="https://www.hackerrank.com/certificates/2d7d83a79116" className="text-lg flex flex-row items-center" target="_blank">
+                            <img src={credentials} alt="credentials" style={{width:"20px",height:"20px"}} />
+                            <span className="text-base ml-2">Problem Solving (Basic) Certificate</span>
+                        </a>
+                        <a href="https://www.hackerrank.com/certificates/2d7d83a79116" className="text-lg flex flex-row items-center mt-2" target="_blank">
+                            <img src={credentials} alt="credentials" style={{width:"20px",height:"20px"}} />
+                            <span className="text-base ml-2">Problem Solving (Basic) Certificate</span>
+                        </a>
+                    </div>
+
+                    {/* LEETCODE CARD */}
+                    <div className="competetive-card">
+                        <div className="flex flex-row items-center">
+                            <img src={LeetcodeLogo} alt="LeetcodeLogo" style={{width:"40px",height:"40px"}} />
+                            <div className="flex flex-col ml-5">
+                                <span className="text-lg font-semibold ">Leetcode</span>
+                                <div className="flex flex-row">
+                                    <img src={star} alt="star" style={{width:"15px",height:"15px"}} />
+                                    <img src={star} alt="star" className="ml-1" style={{width:"15px",height:"15px"}} />
+                                    <img src={star} alt="star" className="ml-1" style={{width:"15px",height:"15px"}} />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row justify-around items-center my-3">
+                            <div className="flex flex-row items-center text-3xl">
+                                3
+                                <img src={star} alt="star" className="ml-1" style={{width:"15px",height:"15px"}} />
+                            </div>
+
+                            <div className="Divide-Line"></div>
+
+                            <div className="flex flex-col">
+                                <span className="text-lg mb-2">Highest Rating</span>
+                                <div className="flex flex-row justify-center text-black font-semibold">
+                                    <div className="Rating" >1</div>
+                                    <div className="Rating ml-2">5</div>
+                                    <div className="Rating ml-2">2</div>
+                                    <div className="Rating ml-2">8</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <p  className="text-lg font-semibold mt-5 mb-2">Badges</p>
+                        <div className="flex flex-row justify-center">
+                            <img src={problemSolvingBadge} alt="problemSolvingBadge" style={{width:"45px",height:"35px"}} />
+                            <img src={CppBadge} alt="CppBadge" className="ml-3" style={{width:"45px",height:"35px"}} />
+                            <img src={JavaBadge} alt="JavaBadge" className="ml-3" style={{width:"45px",height:"35px"}} />
+                            <img src={DaysOfCodingBadge} alt="DaysOfCodingBadge" className="ml-3" style={{width:"45px",height:"35px"}} />
+                        </div>
+
+                        <p className="text-lg text-center font-semibold mt-5 ">250+ Question Solved</p>
+
+                    </div>
+
+                    {/* TODO:CODECHIEF CARD */}
+                    <div className="competetive-card">
+                        <div className="flex flex-row items-center">
+                            <img src={CodeChiefLogo} alt="CodeChiefLogo" style={{width:"40px",height:"40px"}} />
+                            <div className="flex flex-col ml-5">
+                                <span className="text-lg font-semibold ">Codechief</span>
+                                <div className="flex flex-row">
+                                    <img src={star} alt="star" style={{width:"15px",height:"15px"}} />
+                                    <img src={star} alt="star" className="ml-1" style={{width:"15px",height:"15px"}} />
+                                    <img src={star} alt="star" className="ml-1" style={{width:"15px",height:"15px"}} />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row justify-around items-center my-3">
+                            <div className="flex flex-row items-center text-3xl">
+                                3
+                                <img src={star} alt="star" className="ml-1" style={{width:"15px",height:"15px"}} />
+                            </div>
+
+                            <div className="Divide-Line"></div>
+
+                            <div className="flex flex-col">
+                                <span className="text-lg mb-2">Highest Rating</span>
+                                <div className="flex flex-row justify-center text-black font-semibold">
+                                    <div className="Rating" >1</div>
+                                    <div className="Rating ml-2">5</div>
+                                    <div className="Rating ml-2">2</div>
+                                    <div className="Rating ml-2">8</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row justify-around items-center mt-8">
+                            <div className="flex flex-col">
+                                <div className="flex flex-row justify-center text-black font-semibold">
+                                    <div className="Rating" >1</div>
+                                    <div className="Rating ml-2">5</div>
+                                    <div className="Rating ml-2">2</div>
+                                    <div className="Rating ml-2">8</div>
+                                </div>
+                                <span className="text-base mt-2">Global Rating</span>
+                            </div>
+
+                            <div className="Divide-Line" style={{height:"55px"}}></div>
+
+                            <div className="flex flex-col">
+                                <div className="flex flex-row justify-center text-black font-semibold">
+                                    <div className="Rating" >1</div>
+                                    <div className="Rating ml-2">5</div>
+                                    <div className="Rating ml-2">2</div>
+                                    <div className="Rating ml-2">8</div>
+                                </div>
+                                <span className="text-base mt-2">Country Rating</span>
+                            </div>
+                        </div>
+
+
+                        <p className="text-lg text-center font-semibold mt-5">250+ Question Solved</p>
+
+                    </div>
+                    
+                </div>
+
 
                 <div className="flex flex-row mx-28">
                     <div className="w-1/4">
